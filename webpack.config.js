ecc69f406e8e86b41ad182c91671eea6842be6f6
Diego Martinez <diegogama8934@gmail.com>
2024-11-15 12:03:45 -0600

    add: root app created

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "kovan";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+    outputSystemJS: true,
+  });
+
+  return merge(defaultConfig, {
+    // output: {
+    //   filename: 'kovan-root-config.js',
+    //   libraryTarget: 'system', // Use 'system' for SystemJS
+    // },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+  });
+};
